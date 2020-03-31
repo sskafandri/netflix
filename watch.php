26b17bf7f05c1522fe1ba5411177a0d447bbd61e
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-04-01 02:02:13 +0530

    90 displaying the season and episode no.

--- watch.php
+++ watch.php
@@ -22,6 +22,7 @@ $upNextVideo = VideoProvider::getUpNext($con,$video);
         <div class="upNextContainer">
             <h2>Up next:</h2>
             <h3><?php echo $upNextVideo->getTitle(); ?></h3>
+            <h3><?php echo $upNextVideo->getSeasonAndEpisode(); ?></h3>
         </div>
     </div>
     <video controls autoplay>
