a5c219ccd05d07facf9617b28403fab54ad64211
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-04-18 02:31:52 +0530

    97.  Play btn of home pg, query added

--- includes/classes/VideoProvider.php
+++ includes/classes/VideoProvider.php
@@ -36,6 +36,31 @@ class VideoProvider{
         return new Video($con,$row); //object of video class created
     }
 
+
+    public static function getEntityVideoForUser($con,$entityId,$username){
+        $query = $con->prepare("SELECT videoId FROM 'videoProgress'
+                                INNER JOIN videos
+                                ON videoProgress.videoId = videos.id
+                                WHERE videos.entityId= : entityId
+                                AND videoProgress.username = :username
+                                ORDER BY videoProgress.dateModified DESC
+                                LIMIT 1"); 
+        
+        $query->bindValue(":entityId",$entityId);
+        $query->bindValue(":username",$username);
+        $query->execute();
+
+        if($query->rowCount() ==  0){
+            $query = $con->prepare("SELECT * FROM videos 
+                                    WHERE entityId=:entityId
+                                    ORDER BY season, episode ASC LIMIT 1");
+            $query->bindValue(":entityId",$entityId);
+            $query->execute();
+        }
+
+        return $query->fetchColumn();// it will return 1 value that is 1 column in this case
+    }
+
 }
 
 ?>
