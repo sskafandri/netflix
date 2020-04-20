31c4dbc9a477c634c7924fb616541fd2c72934ee
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-04-20 11:39:57 +0530

    114. Searching after 500 milliseconds

--- search.php
+++ search.php
@@ -6,3 +6,22 @@ include_once("includes/header.php")
  </div>
 
  <div class="results"></div>
+
+ <script>
+
+$(function(){
+
+    var username  = '<?php echo $userLoggedIn; ?>';
+    var timer;
+
+    $(".searchInput").keyup(function(){
+        clearTimeout(timer);
+
+        timer = setTimeout(function(){
+            var val = $(".searchInput").val();
+            console.log(val);
+        },500);
+    })
+})
+
+ </script>
