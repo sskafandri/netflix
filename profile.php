1abe907f8439f5f6cfdc846be9a311daffd4cc05
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-04-22 03:25:40 +0530

    119 120. profile page created ,pre filling textbox

---
+++ profile.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once("includes/header.php");
+
+?>
+
+<div class="settingsContainer column">
+
+    <div class="formSection">
+
+        <form action="" method="POST">
+        
+        <h2>User details</h2>
+
+        <?php
+
+        $user = new User($con,$userLoggedIn);
+
+        $firstName = isset($_POST["firstName"]) ? $_POST["firstName"] : $user->getFirstName();
+        $lastName = isset($_POST["lastName"]) ? $_POST["lastName"] : $user->getLastName();
+        $email = isset($_POST["email"]) ? $_POST["email"] : $user->getEmail();
+
+        ?>
+
+        <input type="text" name="firstName" placeholder="First name" value="<?php echo $firstName ?>">
+        <input type="text" name="lastName" placeholder="Last name" value="<?php echo $lastName ?>">
+        <input type="email" name="email" placeholder="Email " value="<?php echo $email ?>">
+
+        <input type="submit" name="saveDetailsButton" value="save">
+        </form>
+
+    </div>
+
+    <div class="formSection">
+
+        <form action="" method="POST">
+        
+        <h2>Update password</h2>
+
+        <input type="password" name="oldPassword" placeholder="Old pasword">
+        <input type="password" name="newPassword" placeholder="New password">
+        <input type="password" name="newPassword2" placeholder="Confirm new password">
+
+        <input type="submit" name="savePasswordButton" value="save">
+        </form>
+
+    </div>
+</div>
